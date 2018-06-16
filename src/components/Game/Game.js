5d4362b2c4de5e7d8f02c92a26165f5c832b6eb0
dacup72 <dacup72@gmail.com>
2018-06-16 13:04:52 -0600

    stuff

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -39,7 +39,7 @@ class Game extends Component {
   handleIncorrectGuess = data => {
     this.setState({
       data: this.resetData(data),
-      score: 0
+      score: this. state ??
     });
   };
 
